98ab1d6fe4cc57741f0ed88ff705f0a9aa156b08
jeffpk <jeffpk@java.net>
2005-08-23 20:59:32 +0000

    Added handlign for unknown windows versions

--- coreAPI/src/java/net/java/games/input/DefaultControllerEnvironment.java
+++ coreAPI/src/java/net/java/games/input/DefaultControllerEnvironment.java
@@ -123,6 +123,11 @@ class DefaultControllerEnvironment extends ControllerEnvironment {
 					pluginClasses = pluginClasses + " net.java.games.input.OSXEnvironmentPlugin";
 				} else if(osName.equals("Windows 98") || osName.equals("Windows 2000") || osName.equals("Windows XP")) {
 					pluginClasses = pluginClasses + " net.java.games.input.DirectInputEnvironmentPlugin";
+				} else if(osName.startsWith("Windows")) {
+					System.out.println("WARNING: Found unknown Windows version: " + osName);
+					System.out.println("Attempting to use default windows plug-in.");
+					System.out.flush();
+					pluginClasses = pluginClasses + " net.java.games.input.DirectInputEnvironmentPlugin";
 				} else {
 					System.out.println("Trying to use default plugin, OS name " + osName +" not recognised");
 				}
