d418a10639f07cf7670a8437932a183350f6527b
endolf <endolf@java.net>
2007-08-02 20:11:20 +0000

    Changed the default plugin for XP to be the combined raw and direct input plugins.

--- coreAPI/src/java/net/java/games/input/DefaultControllerEnvironment.java
+++ coreAPI/src/java/net/java/games/input/DefaultControllerEnvironment.java
@@ -133,15 +133,15 @@ class DefaultControllerEnvironment extends ControllerEnvironment {
 					pluginClasses = pluginClasses + " net.java.games.input.LinuxEnvironmentPlugin";
 				} else if(osName.equals("Mac OS X")) {
 					pluginClasses = pluginClasses + " net.java.games.input.OSXEnvironmentPlugin";
-				} else if(osName.equals("Windows 98") || osName.equals("Windows 2000") || osName.equals("Windows XP")) {
+				} else  if(osName.equals("Windows XP")) {
+					pluginClasses = pluginClasses + " net.java.games.input.DirectAndRawInputEnvironmentPlugin";
+				} else if(osName.equals("Windows 98") || osName.equals("Windows 2000")) {
 					pluginClasses = pluginClasses + " net.java.games.input.DirectInputEnvironmentPlugin";
-//					pluginClasses = pluginClasses + " net.java.games.input.RawInputEnvironmentPlugin";
 				} else if (osName.startsWith("Windows")) {
 					System.out.println("WARNING: Found unknown Windows version: " + osName);
 					System.out.println("Attempting to use default windows plug-in.");
 					System.out.flush();
 					pluginClasses = pluginClasses + " net.java.games.input.DirectInputEnvironmentPlugin";
-//					pluginClasses = pluginClasses + " net.java.games.input.RawInputEnvironmentPlugin";
 				} else {
 					System.out.println("Trying to use default plugin, OS name " + osName +" not recognised");
 				}
@@ -152,6 +152,7 @@ class DefaultControllerEnvironment extends ControllerEnvironment {
 				String className = pluginClassTok.nextToken();					
 				try {
 					if(!loadedPlugins.contains(className)) {
+						System.out.println("Loading: " + className);
 						Class ceClass = Class.forName(className);						
 						ControllerEnvironment ce = (ControllerEnvironment) ceClass.newInstance();
 						if(ce.isSupported()) {
