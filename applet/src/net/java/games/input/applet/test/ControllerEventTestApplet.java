db9ebeccefea8ef3fecf76add773e43342c0a829
endolf <endolf@java.net>
2009-03-22 21:16:20 +0000

    *** empty log message ***

---
+++ applet/src/net/java/games/input/applet/test/ControllerEventTestApplet.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (C) 2003 Jeremy Booth (jeremy@newdawnsoftware.com)
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this 
+ * list of conditions and the following disclaimer. Redistributions in binary 
+ * form must reproduce the above copyright notice, this list of conditions and 
+ * the following disclaimer in the documentation and/or other materials provided
+ * with the distribution. 
+ * The name of the author may not be used to endorse or promote products derived
+ * from this software without specific prior written permission. 
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO 
+ * EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
+ */
+package net.java.games.input.applet.test;
+
+import java.applet.Applet;
+import java.io.IOException;
+
+import net.java.games.input.test.ControllerEventTest;
+import net.java.games.input.applet.JInputAppletResourceLoader;
+
+public class ControllerEventTestApplet extends Applet {
+
+	public void init() {
+		try {
+			new JInputAppletResourceLoader().loadResources(getCodeBase());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		new ControllerEventTest().setVisible(true);
+	}
+}
